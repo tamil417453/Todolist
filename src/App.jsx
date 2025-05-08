525798b2699ad4d31d388722f58ef2c3cecb2773
Tamil selvan <iamtamil.official@gmail.com>
2025-05-08 15:13:38 +0530

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '.Todo-List/Components/Login';
-import Dashboard from './Todo-List/Components/Dashboard';
+import Login from './Todo-Listt/Components/Login';
+import Dashboard from './Todo-Listt/Components/Dashboard';
 
 
 function App() {
